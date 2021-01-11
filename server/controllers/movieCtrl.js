cae892cbd4153ac04ec8621013652073156f2be3
Laura <lz_laura@hotmail.com>
2021-01-11 10:22:46 +0100

    Get All and Get One Movie

--- server/controllers/movieCtrl.js
+++ server/controllers/movieCtrl.js
@@ -1,12 +1,29 @@
 const Movie = require('../models/movieModel');
 const fs = require('fs');
 
-exports.getAllMovie = (req, res, next) => {
-    res.end('Get All Movies');
+
+/**
+ * Return all movie's reviews in the db as JSON objects
+ */
+exports.getAllMovie = (req, res) => {
+    Movie.find()
+        .then((movies) => {
+            res.status(200).json(movies);
+        }).catch((error) => {
+        res.status(400).json({error});
+    });
 };
 
+/**
+ * Return one movie's review in the db as JSON object
+ *
+ * @param req.params.id {String} MongoDB ID of the selected movie
+ */
 exports.getOneMovie = (req, res) => {
-    res.end('Get One Movie');
+    Movie.findOne({_id: req.params.id}
+    ).then((movies) => {
+        res.status(200).json(movies);
+    }).catch((error) => res.status(400).json({error}));
 };
 
 exports.addMovie = (req, res) => {
